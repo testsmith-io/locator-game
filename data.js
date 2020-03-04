f65c5d28e20cb1f8ddfe1115412dd59777ddd10e
Roy de Kleijn <dekleijn.roy@gmail.com>
2020-03-04 10:23:50 +0100

    initial commit

---
+++ data.js
@@ -0,0 +1,269 @@
+var levels = [
+    {
+        instruction: "Select all titles",
+        helpTitle: "Select by tag",
+        selector: {
+            css: "h3"
+        },
+        helpText: "Selects all elements of type <strong>E</strong>. <strong>E</strong> refers to the type of element, for example: <strong>&lt;div&gt;</strong>, <strong>&lt;span&gt;</strong>. ",
+        examples: [
+            'CSS: E',
+            'XPath: //E',
+        ],
+        html: `
+<h3>Title 1</h3>
+<p>Lorem ipsum dolor sit amet.</p>
+<h3>Title 2</h3>
+<p id="description">Consectetur adipiscing elit.</p>
+    `
+    },
+    {
+        instruction: "Select the description text",
+        helpTitle: "Select by id",
+        selector: {
+            css: "p#description"
+        },
+        helpText: "<strong>#id</strong> selects an element with a specific id. You can combine the id selector with the tag selector, resulting in <strong>E#id</strong>",
+        examples: [
+            'CSS: E#id',
+            'XPath: //E[@id = "id"]',
+        ],
+        html: `
+<h3>Title 1</h3>
+<p>Lorem ipsum dolor sit amet.</p>
+<h3>Title 2</h3>
+<p id="description">Consectetur adipiscing elit.</p>
+    `
+    },
+    {
+        instruction: "Select the active list item",
+        helpTitle: "Select by class",
+        selector: {
+            css: "li.active"
+        },
+        helpText: "<strong>.classname</strong> selects an element with a specific classname. You can combine the classname selector with the tag selector, resulting in <strong>E.classname</strong> When combining we are more precise.",
+        examples: [
+            'CSS: E.classname',
+            'XPath: //E[@class = "classname"]',
+        ],
+        html: `
+<ul>
+	<li>item 1</li>
+	<li class="active">item 2</li>
+	<li>item 3</li>
+</ul>
+    `
+    },
+    {
+        instruction: "Select only the button which is part of the toolbar",
+        helpTitle: "Descendant Selector",
+        selector: {
+            css: "div#toolbar button"
+        },
+        helpText: "<strong>E1 E2</strong> selects all the <strong>E2</strong> elements which are inside of <strong>E1</strong> elements. <strong>E2</strong> is descendant, because it's inside another element.",
+        examples: [
+            'CSS: E1 E2',
+            'XPath: //E1/E2',
+        ],
+        html: `
+<p>Some text</p>
+<div id="toolbar">
+	<button type="button">Edit</button> 
+</div>
+<button type="button">Save</button> 
+    `
+    },
+    {
+        instruction: "Select the first div",
+        helpTitle: "Select first of type",
+        selector: {
+            css: "div:first-of-type()"
+        },
+        helpText: "<strong>:first-of-type</strong> selects the first element of a specific type.",
+        examples: [
+            'CSS: E:first-of-type',
+            'XPath: //E[1]',
+        ],
+        html: `
+    <div>First div</div>
+    <div>Second div</div>
+    `
+    },
+    {
+        instruction: "Select the last div",
+        helpTitle: "Select last of type",
+        selector: {
+            css: "div:last-of-type()"
+        },
+        helpText: "<strong>:last-of-type</strong> selects the last element of a specific type.",
+        examples: [
+            'CSS: E:last-of-type',
+            'XPath: //E[last()]',
+        ],
+        html: `
+    <div>First div</div>
+    <div>Second div</div>
+    `
+    },
+    {
+        instruction: "Select the second element from the list",
+        helpTitle: "Select child elements",
+        selector: {
+            css: "ul > li:nth-child(2)"
+        },
+        helpText: "",
+        examples: [
+            'CSS: E:nth-child(2)',
+            'XPath: //*[2][name()="E"]',
+        ],
+        html: `
+<ul>
+    <li>Home</li>
+    <li>Categories</li>
+    <li>Faq</li>
+    <li>Contact</li>
+</ul>
+    `
+    },
+    {
+        instruction: "Select element with text Oceans based on text",
+        helpTitle: "Select elements based on text",
+        selector: {
+            xpath: "//h4[text() = 'Oceans']"
+        },
+        helpText: "XPath's <strong>text()</strong> function can retrieve text from specific elements.",
+        examples: [
+            'CSS: n/a',
+            'XPath: //E[text() = "t"]',
+
+        ],
+        html: `
+    <h4>Midnight</h4>
+    <h4>Another's Arms</h4>
+    <h4>Oceans</h4>
+    <h4>A Sky Full of Stars</h4>
+    `
+    },
+    {
+        instruction: "Select the nickname textfield",
+        helpTitle: "Select elements with attribute exactly containing value",
+        selector: {
+            css: "input[name='nickname']"
+        },
+        helpText: "<strong>E[a='v']</strong> selects all elements that have a specific attribute value.",
+        examples: [
+            'CSS: E[a="v"]',
+            'XPath: //E[@a="v"]',
+        ],
+        html: `
+<form>
+	<input name="j_id819:inputUsername" type="text" placeholder="username" />
+	<input name="nickname" type="text" placeholder="nickname" />
+	<input name="j_id819:inputEmail:j_id819" type="text" placeholder="email" />
+	<input name="inputPassword:j_id819" type="text" placeholder="password" />
+</form>
+    `
+    },
+    {
+        instruction: "Select the username textfield",
+        helpTitle: "Select elements with attribute ending with value",
+        selector: {
+            css: "input[name$='inputUsername']"
+        },
+        helpText: "<strong>E[a='v']</strong> selects all elements ending with a attribute value.",
+        examples: [
+            'CSS: E[a$="v"]',
+            'XPath: //E[ends-with(@A, "v")]',
+        ],
+        html: `
+<form>
+	<input name="j_id819:inputUsername" type="text" placeholder="username" />
+	<input name="nickname" type="text" placeholder="nickname" />
+	<input name="j_id819:inputEmail:j_id819" type="text" placeholder="email" />
+	<input name="inputPassword:j_id819" type="text" placeholder="password" />
+</form>
+    `
+    },
+    {
+        instruction: "Select the email textfield",
+        helpTitle: "Select elements with attribute exactly containing value",
+        selector: {
+            css: "input[name*='inputEmail']"
+        },
+        helpText: "<strong>E[a=*'v']</strong> selects all elements that contain a specific attribute value.",
+        examples: [
+            'CSS: E[a*="v"]',
+            'XPath: //E[contains(@A, "v")]',
+        ],
+        html: `
+<form>
+	<input name="j_id819:inputUsername" type="text" placeholder="username" />
+	<input name="nickname" type="text" placeholder="nickname" />
+	<input name="j_id819:inputEmail:j_id819" type="text" placeholder="email" />
+	<input name="inputPassword:j_id819" type="text" placeholder="password" />
+</form>
+    `
+    },
+    {
+        instruction: "Select the password textfield",
+        helpTitle: "Select elements with attribute starting with value",
+        selector: {
+            css: "input[name^='inputPassword']"
+        },
+        helpText: "<strong>E[a^='v']</strong> selects all elements starting with a attribute value.",
+        examples: [
+            'CSS: E[a^="v"]',
+            'XPath: //E[starts-with(@A, "v")]',
+        ],
+        html: `
+<form>
+	<input name="j_id819:inputUsername" type="text" placeholder="username" />
+	<input name="nickname" type="text" placeholder="nickname" />
+	<input name="j_id819:inputEmail:j_id819" type="text" placeholder="email" />
+	<input name="inputPassword:j_id819" type="text" placeholder="password" />
+</form>
+    `
+    },
+    {
+        instruction: "Select the Edit button related to row 678",
+        helpTitle: "Walking up the DOM",
+        selector: {
+            xpath: "//td[contains(text(), '678')]/../td/button[@class = 'edit']"
+        },
+        helpText: "You can use the XPath 'magic' to walk up the DOM, like: /../",
+        examples: [
+            'CSS: n/a',
+            'XPath: //E1/../E2',
+        ],
+        html: `
+<table class="table table-bordered">
+    <tr>
+        <td>456</td>
+        <td>Description</td>
+        <td>&euro; 100,-</td>
+        <td><button class="edit">Edit</button><button class="delete">Delete</button></td>
+    </tr>
+    <tr>
+        <td>123</td>
+        <td>Description</td>
+        <td>&euro; 100,-</td>
+        <td><button class="edit">Edit</button><button class="delete">Delete</button></td>
+    </tr>
+    <tr>
+        <td>678</td>
+        <td>Description</td>
+        <td>&euro; 100,-</td>
+        <td><button class="edit">Edit</button><button class="delete">Delete</button></td>
+    </tr>
+    <tr>
+        <td>987</td>
+        <td>Description</td>
+        <td>&euro; 100,-</td>
+        <td><button class="edit">Edit</button><button class="delete">Delete</button></td>
+    </tr>
+    </table>
+    `
+    }
+
+
+];
---
+++ exercise.js
@@ -0,0 +1,193 @@
+var level;
+var currentLevel = parseInt(window.localStorage.getItem('currentLevel'), 10) || 0;
+$(document).ready(function () {
+    (function(){
+
+        var tour = new Tour();
+
+        tour.addSteps([
+            {
+                element: "h1",
+                placement: "bottom",
+                backdrop: true,
+                title: "Welcome to the `locator game`!",
+                content: "Find here the assignments you have to complete."
+            },
+            {
+                element: ".html",
+                placement: "bottom",
+                backdrop: true,
+                title: "HTML",
+                content: "This is how the HTML looks like."
+            },
+            {
+                element: "#render",
+                placement: "top",
+                backdrop: true,
+                title: "Rendered",
+                content: "This is how the HTML will look like once rendered."
+            },
+            {
+                element: "#type",
+                placement: "top",
+                backdrop: true,
+                title: "Select type",
+                content: "Select how you want to locate the element. CSS or XPath."
+            },
+            {
+                element: "#expression",
+                placement: "top",
+                backdrop: true,
+                title: "Locate",
+                content: "Type the locator expression."
+            },
+            {
+                element: "#toggleHelp",
+                placement: "bottom",
+                backdrop: true,
+                title: "Help",
+                content: "Click here if you need help."
+            }
+        ]);
+
+        // Initialize the tour
+        tour.init();
+
+        // Start the tour
+        tour.start();
+
+    }());
+
+
+    level = levels[currentLevel];
+    loadLevel();
+
+    setProgress();
+
+    $('pre code').each(function (i, block) {
+        hljs.highlightBlock(block);
+    });
+
+    $('#toggleHelp').click(function (event) {
+        $('#help').toggle();
+    });
+
+    $('#try').click(function (event) {
+        event.preventDefault();
+        var userExpr;
+        if ($("#type").val() === 'XPath') {
+            userExpr = $('#render').find(_x($("[name='expression']").val()));
+        } else {
+            userExpr = $('#render').find($("[name='expression']").val())
+        }
+        userExpr.addClass('highlight');
+        setTimeout(function () {
+            userExpr.removeClass('highlight');
+        }, 500);
+    });
+
+    $('form').submit(function (event) {
+        event.preventDefault();
+        var win = false;
+        var userExpr;
+        var ruleExpr;
+        if (level.selector.css === undefined) {
+            ruleExpr = $('#render').find(_x(level.selector.xpath));
+        } else {
+            ruleExpr = $("#render").find(level.selector.css);
+        }
+
+        if ($("#type").val() === 'XPath') {
+            userExpr = $('#render').find(_x($("[name='expression']").val()));
+        } else {
+            userExpr = $('#render').find($("[name='expression']").val())
+        }
+        userExpr.addClass('highlight');
+
+        win = (userExpr.sequenceEqual(ruleExpr));
+        if (win) {
+            currentLevel++;
+            $.toaster({message: 'Well done!', title: '', priority: 'info'});
+            // $('#console').text("Well done!");
+
+            setProgress();
+
+            if (currentLevel >= levels.length) {
+                $.toaster({message: 'Completed everything!', title: '', priority: 'success'});
+
+                // $('#console').text("Completed everything");
+                window.localStorage.setItem("currentLevel", 0);
+                $("#type").prop("disabled", true);
+                $('input[name="expression"]').prop("disabled", true);
+                $('button').prop("disabled", true);
+                $('#share').modal('toggle');
+            } else {
+                setTimeout(function () {
+                    loadLevel();
+                }, 500);
+            }
+        } else {
+            setTimeout(function () {
+                userExpr.removeClass('highlight');
+                $('input[name="expression"]').val('');
+                // $('#console').text("Please, try agian!");
+                $.toaster({message: 'Please, try again...', title: '', priority: 'warning'});
+            }, 500);
+        }
+    });
+});
+
+$.fn.sequenceEqual = function (compareTo) {
+    if (!compareTo || !compareTo.length || this.length !== compareTo.length) {
+        return false;
+    }
+    for (var i = 0, length = this.length; i < length; i++) {
+        if (this[i] !== compareTo[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+$.fn.contentsEqual = function (compareTo) {
+    return compareTo && this.length === compareTo.length && this.length === this.filter(compareTo).length;
+}
+
+function loadLevel() {
+    // clean form
+    $('#help').hide();
+    $('#type').val('CSS');
+    $('input[name="expression"]').val('');
+    $('#console').text('');
+    $('#helpTitle').text('');
+    $('#helpText').html('');
+    $('#examples').html('');
+
+    window.localStorage.setItem("currentLevel", currentLevel);
+
+    level = levels[currentLevel];
+    $('h1').text(level.instruction);
+    $('#render').html(level.html);
+    $('pre code').text(level.html);
+    $('#helpTitle').text(level.helpTitle);
+    $('#helpText').html(level.helpText);
+    level.examples.forEach(function (item) {
+        $('#examples').append('<div>' + item + '</div>');
+    });
+}
+
+function setProgress() {
+    var progress = 100 / (levels.length / currentLevel);
+    $('.progress-bar').css('width', progress + '%').attr('aria-valuenow', progress).text("Level " + (currentLevel) + " of " + levels.length);
+}
+
+function _x(STR_XPATH) {
+    var xresult = document.evaluate(STR_XPATH, document, null, XPathResult.ANY_TYPE, null);
+    var xnodes = [];
+    var xres;
+    while (xres = xresult.iterateNext()) {
+        xnodes.push(xres);
+    }
+
+    return xnodes;
+}
